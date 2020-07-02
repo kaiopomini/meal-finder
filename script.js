3cb7eadc72c59a8d9b387876e4da96fd41bb68f9
Kaio Pomini <kaiopomini@gmail.com>
2020-07-01 21:37:31 -0300

    Getting random meal and Css styles

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function searchMeal(e){
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
                 if(data.meals === null) {
                     resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
@@ -50,12 +49,25 @@ function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             const meal = data.meals[0];
 
             addMealToDOM(meal);
         });
 };
+
+// Fetch random meal from API
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+        });
+}
 
 // Add meal to DOM
 function addMealToDOM(meal) {
@@ -90,6 +102,7 @@ function addMealToDOM(meal) {
 // Event Listeners
 
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
@@ -104,3 +117,5 @@ mealsEl.addEventListener('click', e => {
         getMealById(mealID);
     }
 });
+
+getRandomMeal()
